4649fd59478df4ad188d789a3df5269be0d4048b
Kyrab <Kyrab@ZAKZRHB-CLLB11.ballito.local>
2022-09-13 08:48:18 +0200

    pat 13sept2022 2143214

--- src/main/java/UI/SignUpScreen.java
+++ src/main/java/UI/SignUpScreen.java
@@ -16,7 +16,7 @@ public class SignUpScreen extends javax.swing.JPanel {
     public SignUpScreen() {
         initComponents();
     }
-
+//ddggd
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
